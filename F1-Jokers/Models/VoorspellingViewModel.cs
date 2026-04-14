4d3da12cae8d9d666e56eed77b31a7452f951455
Daan Maat <daan.maat@windesheim.nl>
2026-04-14 18:25:18 +0200

    Adding F1-Jokers project
    Lokaal gestart, zet het nu op GitHub

---
+++ F1-Jokers/Models/VoorspellingViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace F1Jokers.Models
+{
+    public class VoorspellingViewModel
+    {
+        public string RaceNaam { get; set; } = "Dutch Grand Prix";
+        //later lijst toevoegen van coureurs voor drag & drop
+    }
+}
