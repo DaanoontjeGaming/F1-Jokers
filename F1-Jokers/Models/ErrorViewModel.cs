5350afda4109362bbcbde47c3e25bec5355e03ec
Daan Maat <daan.maat@windesheim.nl>
2026-04-14 18:21:43 +0200

    Revert "Adding F1-Jokers project"
    This reverts commit 33686b51c1addd2f4c97e074fac4b81844a9ddae.

--- F1-Jokers/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace F1Jokers.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- F1-Jokers/Models/VoorspellingViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace F1Jokers.Models
-{
-    public class VoorspellingViewModel
-    {
-        public string RaceNaam { get; set; } = "Dutch Grand Prix";
-        //later lijst toevoegen van coureurs voor drag & drop
-    }
-}
