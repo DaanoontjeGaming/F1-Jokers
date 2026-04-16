0026e524059fe6f33065ca8e0716dbf4757b29de
Daan Maat <daan.maat@windesheim.nl>
2026-04-16 12:35:20 +0200

    Inlogpagina toegevoegd

---
+++ F1-Jokers/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace F1Jokers.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Vul een e-mailadres in.")]
+        [EmailAddress(ErrorMessage = "Dit is geen geldig e-mailadres!")]
+        [Display(Name = "E-mailadres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Wachtwoord is verplicht!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
+        public string Password { get; set; }
+
+        [Display(Name = "Onthoud mij")]
+        public bool RememberMe { get; set; }
+    }
+}
