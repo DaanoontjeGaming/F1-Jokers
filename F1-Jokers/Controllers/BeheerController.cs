a5cd0f2d0f0c25eff3ad6ff9f9179dbb29d1f511
Daan Maat <daan.maat@windesheim.nl>
2026-05-20 17:21:36 +0200

    API connectie gemaakt en JSON omzetten naar bruikbare Data voor Database

--- F1-Jokers/Controllers/BeheerController.cs
+++ F1-Jokers/Controllers/BeheerController.cs
@@ -1,15 +1,40 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using F1Jokers.Services;
 
 namespace F1Jokers.Controllers
 {
-    // De 'uitsmijter' die controleert of de gebruiker de rol 'Beheerder' heeft
     [Authorize(Roles = "Beheerder")]
     public class BeheerController : Controller
     {
+        private readonly F1ApiService _f1ApiService;
+
+        public BeheerController(F1ApiService f1ApiService)
+        {
+            _f1ApiService = f1ApiService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> HaalUitslagenOp()
+        {
+            try
+            {
+                await _f1ApiService.HaalEnVerwerkLaatsteRaceAsync();
+
+                TempData["SuccessMessage"] = "De uitslagen van de laatste race zijn succesvol opgehaald via de F1 API en weggeschreven in de database!";
+            }
+            catch (System.Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Er ging iets mis bij het ophalen van de uitslagen: {ex.Message}";
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
